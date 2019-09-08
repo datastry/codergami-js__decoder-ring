bd6af6034114d203b7f8c6ac31b412923d97dee4
Glitch (datastry-codergami-js--decoder-ring) <none>
2019-09-08 07:14:56 +0000

    create "reference" object

--- decoder-ring.js
+++ decoder-ring.js
@@ -1,3 +1,17 @@
+var reference = {
+  array : {
+    lower : [] ,
+    upper : []
+  } ,
+  charCodes : {
+    a : "a".charCodeAt(),
+    A : "A".charCodeAt(),
+    z : "z".charCodeAt(),
+    Z : "Z".charCodeAt()
+  }
+}
+
+
 var ring = {
   
   encode : function ( pStrPlainText, pNumOffset ) {
